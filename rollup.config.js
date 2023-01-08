dd882f7501577c8c356ae8c89b01499cd991eab3
jelmerveen <jelmerveen@pennions.com>
2023-01-08 15:45:34 +0100

    chore: added multiple outputs correctly

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,19 @@
 import typescript from '@rollup/plugin-typescript';
 
-export default {
+export default [{
     input: 'src/joq.ts',
     output: {
         name: 'joq',
         file: 'dist/joq.js',
         format: 'umd'
     },
+    plugins: [typescript()]
+}, {
+    input: 'src/joq.ts',
     output: {
         name: 'joq',
         file: 'dist/joq.es.js',
         format: 'es'
     },
     plugins: [typescript()]
-};
+}];
